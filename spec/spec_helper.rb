ca1047433d2307477b650afc9666f13669ff0c53
msiwiec <msiwiec@yahoo.com>
2014-07-25 14:56:38 -0600

    put test run in results directory with sub-dir timestamp

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,22 +3,30 @@ $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib/corundum'))
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib/corundum/driver'))
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib/corundum/selenium'))
 
+require 'date'
+require 'fileutils'
 require 'corundum'
 require 'custom_formatter'
 require 'junit_reporter'
 
 RSpec.configure do |config|
+
+  # Create the test report root directory
+  report_root_dir = File.expand_path(File.join(File.dirname(__FILE__), 'reports'))
+  Dir.mkdir(report_root_dir) if not File.exist?(report_root_dir)
+
+  # Create the sub-directory for the test run
+  current_run_report_dir = File.join(report_root_dir, DateTime.now.strftime("%m-%d-%Y_%H_%M_%S"))
+  Dir.mkdir(current_run_report_dir)
+
   # allow it so rspec test cases do not need to have values associated with tagging
   config.treat_symbols_as_metadata_keys_with_true_values = true
 
-  config.add_formatter :documentation, 'output.txt'
-  config.add_formatter CustomFormatter, 'output.html'
-  config.add_formatter JunitReporter, 'output.xml'
+  config.add_formatter :documentation, File.join(current_run_report_dir, 'output.txt')
+  config.add_formatter CustomFormatter, File.join(current_run_report_dir, 'output.html')
+  config.add_formatter JunitReporter, File.join(current_run_report_dir, 'output.xml')
 
-  config.add_setting :screenshot_on_failure, :default => true
-  config.add_setting :command_logging, :default => true
   config.add_setting :test_name, :default => 'Test'
-  config.add_setting :command_delay_sec, :default => 0
 
   config.before(:all) do
 
